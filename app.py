8e3147c4dce8146bedd0d9a02bbc9dcb39a3547d
PA Laboratory <96482887+PAlab0@users.noreply.github.com>
2024-05-06 12:14:55 -0300

    test gpt

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ options = webdriver.ChromeOptions()
 options.add_argument('--no-sandbox')
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
-options.add_argument('--diable-dve-shm-uage')
+options.add_argument('--disable-dev-shm-usage')
 options.binary_location = '/usr/lib/chromium-browser/chromedriver'
 driver = webdriver.Chrome(options=options)
 
