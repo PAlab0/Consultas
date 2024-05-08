02fe6c213591f0b75ffdb6ff67dfa61cb105f07f
PA Laboratory <96482887+PAlab0@users.noreply.github.com>
2024-05-07 22:48:49 -0300

    app.py

--- app.py
+++ app.py
@@ -419,9 +419,7 @@ if servico_sel == "Leitura de PDF":
     uploaded_file = st.sidebar.file_uploader(f"Escolha o seu PDF - {tipo_pdf_sel}", accept_multiple_files=False, type=('pdf'), help=("Coloque um arquivo .pdf"))
 
     if uploaded_file != None:
-        if opcoes_tipo_pdf == "Nomes Faltantes":
-            st.sidebar.button('Processar Excel Completo', type="primary"):
-        else tipo_pdf_sel in opcoes_processamento and opcao_processamento_sel in opcoes_processamento[tipo_pdf_sel]:
+        if tipo_pdf_sel in opcoes_processamento and opcao_processamento_sel in opcoes_processamento[tipo_pdf_sel]:
             if st.sidebar.button('Processar PDF', type="primary"):
                 opcoes_processamento[tipo_pdf_sel][opcao_processamento_sel](uploaded_file)
 
