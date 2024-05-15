96ae4a088a8bcbda9dc1e59a78b63fb0d95b2b8a
PA Laboratory <96482887+PAlab0@users.noreply.github.com>
2024-05-14 22:02:19 -0300

    eee

--- app.py
+++ app.py
@@ -396,16 +396,18 @@ if servico_sel == "Leitura de PDF":
         from selenium.webdriver.chrome.options import Options
         from selenium.webdriver.chrome.service import Service
         from webdriver_manager.chrome import ChromeDriverManager
-        def get_driver():
-        options = Options()
-        options.add_argument('--headless')
-        options.add_argument('--disable-gpu')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
 
-        service = Service(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=service, options=options)
-        return driver
+        def get_driver():
+            
+            options = Options()
+            options.add_argument('--headless')
+            options.add_argument('--disable-gpu')
+            options.add_argument('--no-sandbox')
+            options.add_argument('--disable-dev-shm-usage')
+
+            service = Service(ChromeDriverManager().install())
+            driver = webdriver.Chrome(service=service, options=options)
+            return driver
 
         driver = get_driver()
         driver.get("http://example.com")
