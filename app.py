a61cdfddca2fada611d7c4f6a9c93ea8b375fdd9
PA Laboratory <96482887+PAlab0@users.noreply.github.com>
2024-05-14 22:08:17 -0300

    dede

--- app.py
+++ app.py
@@ -405,7 +405,7 @@ if servico_sel == "Leitura de PDF":
             options.add_argument('--no-sandbox')
             options.add_argument('--disable-dev-shm-usage')
 
-            service = Service(ChromeDriverManager(version='103.0.5060.53').install())
+            service = Service(ChromeDriverManager.install())
             driver = webdriver.Chrome(service=service, options=options)
             return driver
 
