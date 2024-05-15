677fcc4ca9b6b69bfee26af68c09c5c159d35b6a
PA Laboratory <96482887+PAlab0@users.noreply.github.com>
2024-05-14 21:59:06 -0300

    wdwd

--- app.py
+++ app.py
@@ -396,18 +396,16 @@ if servico_sel == "Leitura de PDF":
         from selenium.webdriver.chrome.options import Options
         from selenium.webdriver.chrome.service import Service
         from webdriver_manager.chrome import ChromeDriverManager
-        from webdriver_manager.core.os_manager import ChromeType
-        from webdriver_manager.chrome import ChromeDriverManager
-        from webdriver_manager.core.os_manager import ChromeType
+        '''from webdriver_manager.core.os_manager import ChromeType'''
 
-        
+        '''
         def get_driver():
             return webdriver.Chrome(
                 service=Service(
                     ChromeDriverManager(chrome_type=ChromeDriverManager.ChromeType.CHROMIUM).install()
                 ),
                 options=options,
-            )
+            )'''
 
         options = Options()
         options.add_argument('--headless')
