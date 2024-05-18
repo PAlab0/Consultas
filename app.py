362265680a8482f45dd155debf8319d504e588ae
PA Laboratory <96482887+PAlab0@users.noreply.github.com>
2024-05-18 09:45:43 -0300

    ededed

--- app.py
+++ app.py
@@ -413,11 +413,10 @@ if servico_sel == "Leitura de PDF":
         import streamlit as st
         import os, sys
         
-        def installff():
-            os.system('sbase install geckodriver')
-            os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+        
+        os.system('sbase install geckodriver')
+        os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
 
-        _ = installff()
         from selenium import webdriver
         from selenium.webdriver import FirefoxOptions
         opts = FirefoxOptions()
